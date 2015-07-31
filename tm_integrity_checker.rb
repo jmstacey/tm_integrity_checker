b4438520f237e42d1029c5a5e8a50d9a4d1429fb
Jon Stacey <jon@jonsview.com>
2015-07-31 13:55:17 -0600

    Misc cleanup on tm_integrity_checker.rb

--- tm_integrity_checker.rb
+++ tm_integrity_checker.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby 
+#!/usr/bin/env ruby
 
 # =TM Integrity Checker Program Overview
 #
@@ -18,7 +18,7 @@
 # (1) Looks at every file on the backup and compares its sha1 hash with the corresponding live file sha1 hash.
 # (2) If a mismatch is found, the mtime's are checked. If the mtimes are the same, it is assumed that any changes were unintentional and discrepency is reported to the user.
 
-# This program compares 
+# This program compares
 # This is a small script to transfer my latest work between my 2 macs in a somewhat automated and controlled manner.
 #
 # ==Usage
@@ -42,9 +42,9 @@ require 'find'
 require 'digest/sha1'
 
 ACCOUNT_NAME        = "Jon Stacey\u2019s iMac"   # Name of your Mac
-DRIVE_NAME          = "Macintosh HD"             # Name of your primary hard drive
+DRIVE_NAME          = "Fusion"             # Name of your primary hard drive
 STARTING_DIRECTORY  = 'Users/Jon'                # The starting directory (if you don't want to start at the root level) [INCLUDE prefixed forward slash]
-TIME_MACHINE_NAME   = "Time Machine Backups"     # The name of your Time Machine Backup drive
+TIME_MACHINE_NAME   = "Time Machine 2"     # The name of your Time Machine Backup drive
 
 TIME_MACHINE_PATH   = File.join('/Volumes/', TIME_MACHINE_NAME, '/Backups.backupdb/', ACCOUNT_NAME, '/Latest/', DRIVE_NAME, '/', STARTING_DIRECTORY, '/')
 
@@ -63,7 +63,7 @@ def hash(file)
     puts "Unexpected error reading #{file}. Skipping."
     return nil
   end
-  
+
   digest.hexdigest
 end
 
@@ -71,7 +71,7 @@ def main
   count = 0
   Find.find(TIME_MACHINE_PATH) do |backup_file|
     live_file = File.join('/', STARTING_DIRECTORY, '/', backup_file.sub(TIME_MACHINE_PATH, ''))
-    
+
     next if File.directory?(backup_file) || File.directory?(live_file)
     next if !File.exists?(live_file) # it obviously exists on the backup if we're here
     count += 1
@@ -79,22 +79,22 @@ def main
     # Get sha1 hashes
     live_file_hash = String.new
     backup_file_hash = String.new
-    
+
     # Assumption: the live file and backup file are on different physical devices.
     t1 = Thread.new { live_file_hash = hash(live_file) }
     t2 = Thread.new { backup_file_hash = hash(backup_file) }
     t1.join
     t2.join
-    
+
     next if live_file_hash.nil? || backup_file_hash.nil? # Unexpected error, so skip
-    
+
     # Potential mismatch
     if live_file_hash != backup_file_hash
       puts "Found mismatch on #{live_file}." if File.mtime(live_file) == File.mtime(backup_file)
     end
-    
+
   end
-  
+
   25.times { print '-' }
   puts "\nChecked the integrity of #{count} files total."
 end
